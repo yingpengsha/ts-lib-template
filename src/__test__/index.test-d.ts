aa51590ffe973946493a6fbed2ad85e0018c77cd
yingpengsha <yingpengsha@kuaishou.com>
2024-01-11 21:02:53 +0800

    chore: remove type check

--- src/__test__/index.test-d.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expectTypeOf } from 'vitest'
-import func from '@/index'
-
-describe('index.ts test', () => {
-  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-  expectTypeOf(func).returns.toEqualTypeOf<void>()
-})
